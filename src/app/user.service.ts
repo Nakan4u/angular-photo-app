629a25ea130ab68cad2c878efc35bb393f8dbbab
Григорий Наконечный <nakan88@gmail.com>
2018-01-27 17:26:52 +0200

    add Observable to the canActivate guard;

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,40 +1,29 @@
 import { Injectable } from '@angular/core';
-import {CanActivate} from "@angular/router";
+import { CanActivate } from "@angular/router";
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class UserService {
-  // private isLoggedIn: boolean = false;
+  private isLoggedInObs = new BehaviorSubject<boolean>(false);
 
-  private isLoggedIn = new BehaviorSubject<boolean>(false);
-  getLoggedInStatus = this.isLoggedIn.asObservable();
+  getLoggedInStatus = this.isLoggedInObs.asObservable();
 
   constructor() { }
 
-  // public getLoggedInStatus(): boolean {
-  //   return this.isLoggedIn;
-  // }
   public logIn() {
-    // this.isLoggedIn = true;
-    this.isLoggedIn.next(true)
+    this.isLoggedInObs.next(true)
   }
   public logOut() {
-    // this.isLoggedIn = false;
-    this.isLoggedIn.next(false)
+    this.isLoggedInObs.next(false)
   }
 }
 
 @Injectable()
-export class OnlyLoggedInUsersGuard implements CanActivate { 
-  constructor(private userService: UserService) {}; 
+export class OnlyLoggedInUsersGuard implements CanActivate {
+  constructor(private userService: UserService) { };
 
   canActivate() {
     console.log("OnlyLoggedInUsers");
-    if (1) { 
-      return true;
-    } else {
-      window.alert("You don't have permission to view this page"); 
-      return false;
-    }
+    return this.userService.getLoggedInStatus;
   }
 }
