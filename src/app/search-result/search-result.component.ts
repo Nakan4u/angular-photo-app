c27be6df9d76f2cacfa9474dffc16e5eb078087a
Григорий Наконечный <nakan88@gmail.com>
2018-01-30 23:23:27 +0200

    add show more button to search page;

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { SearchItem } from '../search.service';
 
 @Component({
@@ -9,10 +9,22 @@ import { SearchItem } from '../search.service';
 })
 export class SearchResultComponent implements OnInit {
   @Input('results') results: SearchItem[];
+  private term: string;
+  private type: string;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params['term']) {
+        this.term = params['term'];
+      }
+      if (params['type']) {
+        this.type = params['type'];
+      }
+    })
   }
 
   searchByTag(tag: string, event) {
