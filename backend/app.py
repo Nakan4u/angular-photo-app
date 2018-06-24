12537938028a15593af02bee7814c8b5b9fabaa7
Григорий Наконечный <nakan88@gmail.com>
2018-06-24 17:43:15 +0300

    tune base session auth logic

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,36 @@
-from flask import Flask, jsonify, g
-import sqlite3
+from flask import Flask, jsonify, g, session, redirect, url_for, escape, request
+from helpers import query_db, login_required
+
+import os
+
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-# db helpers
-DATABASE = './test.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-# end db helpers
 
 @app.route('/')
 def hello():
+    session['userId'] = 'Nakan'
     # return jsonify({'text':'Hello World!'})
+    
     users = query_db("SELECT * from 'users'")
     return jsonify(users)
+
+@app.route('/login')
+def login():
+    if 'userId' in session:
+        userId = session['userId']
+        return 'Hello' + userId
+    
+    return 'You are not logged in'
+
+@app.route('/logout')
+@login_required
+def logout():
+   # remove the userId from the session if it is there
+   session.pop('userId', None)
+   return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/helpers.py
@@ -0,0 +1,39 @@
+from flask import g, redirect, render_template, request, session
+from functools import wraps
+
+import sqlite3
+
+# db helpers
+DATABASE = './test.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+# end db helpers
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
